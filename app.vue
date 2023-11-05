019a78f4f7f475b30c468582a2280e574a8631a8
Ismail Hassan <code.ismailah28@gmail.com>
2023-11-05 01:33:40 +0100

    scaffold project with board structure

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <img
+        src="https://vueschool.io/img/logo/vueschool_logo_multicolor_negative.svg"
+        width="200"
+        class="mr-3"
+        alt="Vue School"
+      />
+      Trello Board
+    </h1>
+    <TrelloBoard />
+  </div>
+</template>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { nanoid } from "nanoid";
+import type { Column } from "@/types";
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: "Baclkog",
+    tasks: [
+      {
+        id: nanoid(),
+        title: "Create marketing landing page",
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: "Develop cool new feature",
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: "Fix page nav bug",
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: "Selected for Dev",
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: "In Progress",
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: "QA",
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: "Complete",
+    tasks: [],
+  },
+]);
+</script>
+<template>
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div
+      v-for="column in columns"
+      :key="column.id"
+      class="column bg-gray-200 p-5 rounded min-w-[250px]"
+    >
+      <header class="font-bold mb-4">
+        {{ column.title }}
+      </header>
+      <TrelloBoardTask v-for="task in column.tasks" :task="task" />
+      <footer>
+        <button class="text-gray-500">+ Add a Card</button>
+      </footer>
+    </div>
+  </div>
+</template>
